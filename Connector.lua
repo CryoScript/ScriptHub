9fc3e0dfd64f896bd8648583b837900c606d63d2
CryoScript <140853779+CryoScript@users.noreply.github.com>
2024-06-20 20:04:12 -0500

    Update Connector.lua

--- Connector.lua
+++ Connector.lua
@@ -1,11 +1,11 @@
 local NotificationModule = loadstring(game:HttpGet("https://raw.githubusercontent.com/CryoScript/CryoHublib/main/Notification"))() -- booting notifications
 -- script holder:
 if game.PlaceId == 3276265788 then
-    loadstring(game:HttpGet(('___'),true))() -- Legends of speed
+    loadstring(game:HttpGet(('https://raw.githubusercontent.com/CryoScript/ScriptHub/main/Legends%20of%20Speed.lua'),true))() -- Legends of speed
 elseif game.PlaceId == 3101667897 then
-    loadstring(game:HttpGet(('___'),true))() -- Legends of speed
+    loadstring(game:HttpGet(('https://raw.githubusercontent.com/CryoScript/ScriptHub/main/Legends%20of%20Speed.lua'),true))() -- Legends of speed
 elseif game.PlaceId == 3232996272 then
-    loadstring(game:HttpGet(('___'),true))() -- Legends of speed
+    loadstring(game:HttpGet(('https://raw.githubusercontent.com/CryoScript/ScriptHub/main/Legends%20of%20Speed.lua'),true))() -- Legends of speed
 elseif game.PlaceId == 205224386 then
     loadstring(game:HttpGet(('___'),true))() -- Hide and Seek
 elseif game.PlaceId ==  3623096087 then
