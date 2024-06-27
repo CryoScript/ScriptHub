b9b23ce329429545b8954bcddec2bec2a490650e
CryoScript <140853779+CryoScript@users.noreply.github.com>
2024-06-27 03:33:32 -0500

    Update Connector.lua

--- Connector.lua
+++ Connector.lua
@@ -10,7 +10,7 @@ elseif game.PlaceId == 205224386 then
     loadstring(game:HttpGet(('https://raw.githubusercontent.com/CryoScript/ScriptHub/main/Hide%20%26%20Seek%20Extreme.lua'),true))() -- Hide & Seek Extreme
 elseif game.PlaceId ==  3623096087 then
     loadstring(game:HttpGet(('https://raw.githubusercontent.com/CryoScript/ScriptHub/main/Muscle%20Legends.lua'),true))() -- Muscle Legends
-    elseif game.PlaceId ==  12345678910 then
+    elseif game.PlaceId ==  4746533355 then
     loadstring(game:HttpGet(('____________'),true))() -- Lights Out
 else
     NotificationModule.ShowNotification("Uh Oh!", "Game Not Supported Yet!", 7)
